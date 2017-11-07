8c2fe2bcbba67a54d8b0df555c73da7b00cbf85d
howtojazz <swchowtojazz@gmail.com>
2017-11-07 17:56:01 +0900

    Initial Commit

---
+++ SoundWaveViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundWaveViewer
+//
+//  Created by Won Cheul Seok on 2017. 11. 7..
+//  Copyright © 2017년 Evan Seok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
