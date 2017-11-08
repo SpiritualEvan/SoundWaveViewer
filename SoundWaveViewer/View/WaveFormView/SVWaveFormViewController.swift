bac7c0ab2a63db71b487c1b107b7ac75346c4978
howtojazz <swchowtojazz@gmail.com>
2017-11-08 18:11:09 +0900

    Added main view

--- SoundWaveViewer/View/WaveFormView/SVWaveFormViewController.swift
+++ SoundWaveViewer/View/WaveFormView/SVWaveFormViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class SVWaveFormViewController: UIViewController {
 
-    @IBOutlet weak var tracksView: UITableView!
-    @IBOutlet weak var waveFormView: UIScrollView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
