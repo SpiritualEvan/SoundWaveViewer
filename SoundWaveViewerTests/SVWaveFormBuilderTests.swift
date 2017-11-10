9d71fc0d3dd1566f310cabb3efef1ea4edeaa005
howtojazz <swchowtojazz@gmail.com>
2017-11-10 22:24:54 +0900

    Added wave form builder

--- SoundWaveViewerTests/SVWaveFormBuilderTests.swift
+++ SoundWaveViewerTests/SVWaveFormBuilderTests.swift
@@ -15,6 +15,7 @@ class SVWaveFormBuilderTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        self.continueAfterFailure = false
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -28,7 +29,7 @@ class SVWaveFormBuilderTests: XCTestCase {
         // test for multi track audios
         let onNextExpectation = expectation(description: "onNextExpectation")
         let mediaPath = Bundle(for: type(of: self)).path(forResource: "1", ofType: "m4a")
-        SVWaveFormBuilder.buildWaveform(mediaURL: URL(fileURLWithPath:mediaPath!)) { (waveform, error) in
+        SVWaveFormBuilder.buildWaveform(mediaURL: URL(fileURLWithPath:mediaPath!), briefWaveformWidth:320) { (waveform, error) in
             XCTAssertNotNil(waveform)
             XCTAssertNil(error)
             XCTAssertNotNil(waveform!.pcmDatas)
@@ -41,6 +42,32 @@ class SVWaveFormBuilderTests: XCTestCase {
                 XCTFail((error?.localizedDescription)!)
                 return
             }
+        }
+    }
+    func testDownsampling1HourSoundA() {
+        let floatArray = [Float32](repeating:1.0, count: 44100 * 60 * 60)
+        self.measure {
+            let downsampleArray = SVWaveFormBuilder.downsamplePCMDatas(pcmDatas: floatArray, widthInPixel: 10)
+            XCTAssertNotNil(downsampleArray)
+            XCTAssertEqual(10, downsampleArray!.count)
+            
+            // vDSP_desamp doesn't calculate accurately
+            XCTAssertTrue(0.1 > fabs(downsampleArray!.first! - 1.0)) // expect 1.0
+            XCTAssertTrue(0.1 > fabs(downsampleArray!.last! - 1.0)) // expect 1.0
+        }
+        
+    }
+    func testDownsampling1HourSoundB() {
+        var dataArray = [Float32](repeating:1.0, count: 44100 * 60 * 30)
+        dataArray += [Float32](repeating:-1.0, count: 44100 * 60 * 30)
+        self.measure {
+            let floatArray = SVWaveFormBuilder.downsamplePCMDatas(pcmDatas: dataArray, widthInPixel: 20)
+            XCTAssertNotNil(floatArray)
+            XCTAssertEqual(20, floatArray!.count)
+            XCTAssertTrue(0.1 > fabs(floatArray!.first! - 1.0)) // expect 1.0
+            XCTAssertTrue(0.1 > fabs(floatArray![9] - 1.0)) // expect 1.0
+            XCTAssertTrue(0.1 > fabs(floatArray![10] + 1.0)) // expect -1.0
+            XCTAssertTrue(0.1 > fabs(floatArray!.last! + 1.0)) // expect -1.0
         }
     }
 }
